c57eb05769bfac9ee1628265a2b904927afc626c
EKANT KHUSHAL KAPGATE <2016bcs105@sggs.ac.in>
2025-03-30 17:28:50 +0530

    Add files via upload

---
+++ LoadBalancer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+namespace LoadBalancer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress host = IPAddress.Parse("127.0.0.1");
+
+            Task.Run(() => LoadBalancer.HealthChecker());
+
+            LoadBalancer.StartLoadBalancer(host);
+        }
+    }
+}
